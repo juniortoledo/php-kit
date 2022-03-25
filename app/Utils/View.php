6f4541c21cf5fa0f6ec69645be788c38a9011c6a
juniortoledo <36316001+juniortoledo@users.noreply.github.com>
2022-03-25 11:31:10 -0300

    Update View.php

--- app/Utils/View.php
+++ app/Utils/View.php
@@ -2,7 +2,6 @@
 
 namespace App\Utils;
 
-use App\Models\CrudModel;
 use League\Plates\Engine;
 
 class View
@@ -13,15 +12,5 @@ class View
   public function __construct()
   {
     $this->view = new Engine(__DIR__ . '/../views');
-    $this->crud = new CrudModel();
-
-    if (isset($_SESSION['user'])) {
-      foreach ($this->crud->read('users', 'id', $_SESSION['id']) as $item) {
-        $this->view->addData([
-          'urlName' => str_replace('route=/', '', $_SERVER['QUERY_STRING']),
-          'avatar' =>  $item->cover
-        ]);
-      }
-    }
   }
 }
